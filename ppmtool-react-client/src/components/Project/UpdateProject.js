6d953cd6ff82e0757bcc959cd7aca82a30c772b3
Chung <jaehoonch@hotmail.com>
2019-12-01 01:00:42 +0900

    added project update

---
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UpdateProject extends Component {
+    render() {
+        return (
+            <div>sdfsd</div>
+        )
+    }
+}
+
+export default UpdateProject;
