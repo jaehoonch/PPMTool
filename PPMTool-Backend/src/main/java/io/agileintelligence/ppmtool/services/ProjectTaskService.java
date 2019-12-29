b87ef3d0cac98cea2f822587cddbc2f8547542de
cdh <jaehoon92@gmail.com>
2019-12-29 15:02:34 +0900

    Added addProjectTask functionality

--- PPMTool-Backend/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMTool-Backend/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -43,7 +43,7 @@ public class ProjectTaskService {
             projectTask.setProjectIdentifier(projectIdentifier);
 
             // INITIAL priority when priority null
-            if(projectTask.getPriority() == null){ // In the future we need projectTask.getPriority() == 0 to handle the form
+            if(projectTask.getPriority() == 0 ||  projectTask.getPriority() == null){ // In the future we need projectTask.getPriority() == 0 to handle the form
                 projectTask.setPriority(3);
             }
             // INITIAL status when status is null
